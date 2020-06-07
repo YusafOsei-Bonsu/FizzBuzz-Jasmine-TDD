4863c2f8d43f7124250c34485676f80d3f901ed7
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-07 18:52:12 +0100

    Testing the scenario where the value is ONLY divisible by 3

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -8,4 +8,7 @@ describe('FizzBuzz', () => {
 
     // Testing Fizzbuzz with a number (i.e. 30) that's divisible by 3 and 5
     it("Should return 'FizzBuzz'", () => expect(FizzBuzz(30)).toEqual("FizzBuzz"));
+
+    // Testing Fizzbuzz with a number (i.e. 6) that's only divisible by 3
+    it("Should return 'Fizz'", () => expect(FizzBuzz(6)).toEqual("Fizz"));
 });
